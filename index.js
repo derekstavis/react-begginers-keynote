87e10c52d37513d888c703256d3e9fb908cc010f
Derek Stavis <dekestavis@gmail.com>
2016-03-05 13:53:52 -0300

    Implement a counter to exemplify explicit state changes

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class YayItWorks extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { counter: 0 }
+  }
+
   handleButton() {
-    alert('Yay! It works!')
+    this.setState({ counter: this.state.counter + 1 })
   }
 
   render() {
     return (
       <div>
+        <h1>A counter example</h1>
+        <div>Current value is {this.state.counter}</div>
         <button onClick={this.handleButton.bind(this)}>
-          Click Me!
+          Increase
         </button>
       </div>
     )
